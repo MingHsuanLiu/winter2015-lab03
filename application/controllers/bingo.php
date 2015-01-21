2894ed3510980f83e4d4cf7b9682aa1eebd4f10b
a00770012 <bcit@bcit-PC>
2015-01-20 16:57:36 -0800

    Added new method wisdom() to display sixth person. Signed-off-by:a00770012 <bcit@bcit-PC>

--- application/controllers/bingo.php
+++ application/controllers/bingo.php
@@ -29,7 +29,18 @@ class Bingo extends Application {
         // build the list of authors, to pass on to our view
         $source = $this->quotes->get('5');
         $authors = array();
-        $authors[] = array('who' => $source['who'], 'mug' => $source['mug'], 'href' => $source['where'],'what' => $source['what']);
+        $authors[] = array('who' => $source['who'], 'mug' => $source['mug'], 'href' => $source['where'], 'what' => $source['what']);
+        $this->data = array_merge($authors[0], $this->data);
+
+        $this->render();
+    }
+
+    function wisdom() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get('6');
+        $authors = array();
+        $authors[] = array('who' => $source['who'], 'mug' => $source['mug'], 'href' => $source['where'], 'what' => $source['what']);
         $this->data = array_merge($authors[0], $this->data);
 
         $this->render();
